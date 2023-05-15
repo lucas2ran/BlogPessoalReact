fe3d31fe1acf947bcda60cd2e295fba82aaaebe4
lucas vinicius <lucas.thuran@hotmail.com>
2023-05-15 09:33:18 -0300

    Estilizando a pagina inicial

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='inicio' >Bem vindo!!!</h1>
+            <img src="https://kanto.legiaodosherois.com.br/w728-h381-gnw-cfill-gcc-f:fbcover/wp-content/uploads/2022/04/legiao_OZTlaWFfI0Km.jpg.webp" className='img'/>
+        </>
+    );
+}
+
+export default Home;
