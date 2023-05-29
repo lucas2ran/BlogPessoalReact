5a23a884ec7bc220f3686787abd02c7915d4aadd
lucas vinicius <lucas.thuran@hotmail.com>
2023-05-29 15:52:27 -0300

    redux

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -5,40 +5,42 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import  InstagramIcon from '@mui/icons-material/Instagram';
 import { useSelector } from 'react-redux';
 import { TokenState } from '../../../store/tokens/TokensReducer';
 
+
 function Footer() {
     const token = useSelector<TokenState, TokenState["tokens"]>(
-        (state) => state.tokens
-      );
-      
+      (state) => state.tokens
+    );
+  
+    // if ternario
     return (
-        <footer className='footer'>
-            <Grid container py={4} alignItems={'center'}>
-                <Grid item xs={4} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-                    <Typography variant='h6'>Blog Pessoal - Lucas Vinicius</Typography>
-                    <Box display={'flex'}>
-                        <Typography></Typography>
-                        <KeyboardArrowLeftIcon />
-                        <Typography color={'lightgrey'} >React / MUI</Typography>
-                        <KeyboardArrowRightIcon />
-                    </Box>
-                </Grid>
-                <Grid item xs={4} display={'flex'} justifyContent={'center'}>
-                    <Box display={'flex'} gap={2} alignItems={'center'} className='iconesFooter'>
-                        <GitHubIcon fontSize='inherit' className='iconeInd' />
-                        <LinkedInIcon fontSize='inherit' className='iconeInd' />
-                        <InstagramIcon fontSize='inherit' className='iconeInd' />
-                    </Box>
-                </Grid>
-                <Grid item xs={4} display={'flex'} justifyContent={'center'}>
-                    <Typography variant='h5'>Apoio: Generation Brasil</Typography>
-                </Grid>
-            </Grid>
-        </footer>
+      <footer className='footer'>
+        <Grid container py={4} alignItems={'center'}>
+          <Grid item xs={4} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
+            <Typography variant='h6' align='center'>Blog da turma 63 - Feito por: Thiago Faccipieri - 2023</Typography>
+            <Box display={'flex'}>
+              <Typography>Feito com:</Typography>
+              <KeyboardArrowLeftIcon />
+              <Typography color={'lightgrey'} >React / MUI</Typography>
+              <KeyboardArrowRightIcon />
+            </Box>
+          </Grid>
+          <Grid item xs={4} display={'flex'} justifyContent={'center'}>
+            {/* controlando o que aparece em tela ou não, de acordo com o token da pessoa, com um if ternário */}
+            {token !== '' ? <Box display={'flex'} gap={2} alignItems={'center'} className='iconesFooter'>
+            <GitHubIcon fontSize='inherit' className='iconeInd' />
+              <GitHubIcon fontSize='inherit' className='iconeInd' />
+              <LinkedInIcon fontSize='inherit' className='iconeInd' />
+            </Box> : <>você não está logado ainda</>}
+          </Grid>
+          <Grid item xs={4} display={'flex'} justifyContent={'center'}>
+            <Typography variant='h5'>Em parceria com: Generation Brasil</Typography>
+          </Grid>
+        </Grid>
+      </footer>
     )
-}
-
-export default Footer;
+  }
+  
+  export default Footer
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -3,91 +3,93 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
 import './Navbar.css'
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { TokenState } from '../../../store/tokens/TokensReducer';
 import { addToken } from '../../../store/tokens/Actions';
+import { Grid } from '@mui/material';
 
 function Navbar() {
     const token = useSelector<TokenState, TokenState["tokens"]>(
-        (state) => state.tokens
-      );
-    
-      const dispatch = useDispatch()
-      const navigate = useNavigate();
-
-    function goLogout(){
-        alert("Usuário deslogado")
-        dispatch(addToken(''))
-        navigate('/login')
-    }
-
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar className='NavBar' position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                
-                    </IconButton>
-                    <Box className='textdecorater'>
-                    <Link to="/home" className='textdecorater'>
-                        <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-                            BlogPessoalShock
-                        </Typography>
-                    </Link>
-                    </Box>
-
-                    <Box display="flex" justifyContent="start">
-                        <Link to="/home" style={{textDecoration: "none"}}>
-                            <Box className='navigation' mx={1} style={{ cursor: "pointer" }}>
-                                <Typography variant="h6" color="inherit">
-                                    Início
-                                </Typography>
-                            </Box>
-                        </Link>
-                    </Box>
-                    <Link to="/postagem" className='textdecorater'>
-                    <Box className='navigation' mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                Postagens
-                            </Typography>
-                        
-                    </Box>
-                    </Link>
-                    <Link to="/temas" className='textdecorater'>
-                    <Box className='navigation' mx={1} style={{ cursor: "pointer" }}>
-                        <Box>
-                            <Typography variant="h6" color="inherit">
-                                Tema
-                            </Typography>
-                        </Box>
-                    </Box>
-                    </Link>
-                    <Link to="/formularioTema" className='textdecorater'>
-                    <Box className='navigation' mx={1} style={{ cursor: "pointer" }}>
-                        
-                            <Typography variant="h6" color="inherit">
-                                Cadastrar tema
-                            </Typography>
-                        
-                    </Box>
-                    </Link>
-                    <Box className='navigation' mx={1} style={{ cursor: "pointer" }} onClick={goLogout} >
-                    <Typography variant="h6" color="inherit">
-                          Logout
-                    </Typography>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-        </Box>
+      (state) => state.tokens
     );
-}
-export default Navbar;
+  
+    const dispatch = useDispatch()
+    const navigate = useNavigate();
+  
+    function logout() {
+      alert('Usuário deslogado com sucesso');
+      dispatch(addToken(''))
+      navigate('/login');
+    }
+  
+    let navbarComponent;
+  
+    if(token !== '') {
+      navbarComponent = (
+  <AppBar position="static" className="navbar">
+          <Toolbar variant="dense">
+            <Grid container justifyContent={'space-between'} className='fonte'>
+              <Box style={{ cursor: 'pointer' }}>
+                <Typography variant="h5" color="inherit" className='fonte'>
+                  BlogPessoal
+                </Typography>
+              </Box>
+  
+              <Box display="flex" justifyContent="start">
+                <Link to="/home">
+                  <Box mx={1} style={{ cursor: 'pointer' }}>
+                    <Typography variant="h6" color="inherit">
+                      home
+                    </Typography>
+                  </Box>
+                </Link>
+                <Link to="/postagens">
+                  <Box mx={1} style={{ cursor: 'pointer' }}>
+                    <Typography variant="h6" color="inherit">
+                      postagens
+                    </Typography>
+                  </Box>
+                </Link>
+                <Link to="/temas">
+                  <Box mx={1} style={{ cursor: 'pointer' }}>
+                    <Typography variant="h6" color="inherit">
+                      temas
+                    </Typography>
+                  </Box>
+                </Link>
+                <Link to="/formularioTema">
+                  <Box mx={1} style={{ cursor: 'pointer' }}>
+                    <Typography variant="h6" color="inherit">
+                      cadastrar tema
+                    </Typography>
+                  </Box>
+                </Link>
+                <Link to="/perfil">
+                  <Box mx={1} style={{ cursor: 'pointer' }}>
+                    <Typography variant="h6" color="inherit">
+                      perfil
+                    </Typography>
+                  </Box>
+                </Link>
+                <Box mx={1} style={{ cursor: 'pointer' }} onClick={logout}>
+                  <Typography variant="h6" color="inherit">
+                    logout
+                  </Typography>
+                </Box>
+              </Box>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      )
+    }
+  
+    return (
+      <>
+        {navbarComponent}
+      </>
+    );
+  }
+  
+  export default Navbar;
